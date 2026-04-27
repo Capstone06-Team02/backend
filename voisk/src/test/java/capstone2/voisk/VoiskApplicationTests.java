2641d13eecea60a10eb5dfef6b99ee2630424d08
keypang <bestnest820@naver.com>
2026-04-27 23:50:26 +0900

    [Init] inital setting

---
+++ voisk/src/test/java/capstone2/voisk/VoiskApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone2.voisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
