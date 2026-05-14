526bf4e8876ddb27a5f931c59cf6cf0d6739d314
keypang <bestnest820@naver.com>
2026-05-14 11:03:08 +0900

    [Feat] Store 관련 Entity 생성

---
+++ voisk/src/main/java/capstone2/voisk/entity/OptionGroup.java
@@ -0,0 +1,47 @@
+package capstone2.voisk.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "option_group")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class OptionGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "option_group_id")
+    private Long id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "is_required")
+    private Boolean isRequired;
+
+    @Column(name = "min_select")
+    private Integer minSelect;
+
+    @Column(name = "max_select")
+    private Integer maxSelect;
+
+    @Column(name = "is_available")
+    private Boolean isAvailable;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu id", nullable = false)
+    private Menu menu;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_option_item id", nullable = false)
+    private OptionItem parentOptionItem;
+
+    @OneToMany(mappedBy = "optionGroup")
+    private List<OptionItem> optionItems = new ArrayList<>();
+}
